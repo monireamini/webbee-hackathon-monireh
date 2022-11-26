b8ed01eb37d88d18e0df125e50bc1d6471ee0a9b
Monire Amini <monireamini@gmail.com>
2022-11-26 11:07:13 +0330

    add react-navigation

---
+++ src/modules/dashboard/dashboard.screen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const DashboardScreen = () => {
+  return <Text>Dashboard Screen</Text>;
+};
+
+export default DashboardScreen;
---
+++ src/modules/manage-categories/manage-categories.screen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from "react-native";
+
+const ManageCategoriesScreen = () => {
+  return <Text>Manage Categories Screen</Text>;
+};
+
+export default ManageCategoriesScreen;
