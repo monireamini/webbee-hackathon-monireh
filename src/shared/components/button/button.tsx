6fe6eec8ac7541b11b1aa4a0280e1a0edfe46de3
Monire Amini <monireamini@gmail.com>
2022-11-27 03:07:30 +0330

    add active opacity to pressables

--- src/shared/components/button/button.tsx
+++ src/shared/components/button/button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Pressable, Text} from 'react-native';
+import {Animated, Pressable, Text} from 'react-native';
 import {buttonPropTypes} from './button.types';
 import styles from './button.styles';
 import {colors, dimensions, margins} from '../../styles/styles';
@@ -12,11 +12,33 @@ const CustomButton = (props: buttonPropTypes) => {
     width = dimensions.windowWidth - 2 * margins.pageHorizontal,
   } = props;
 
+  const animated = new Animated.Value(1);
+
+  function fadeIn() {
+    Animated.timing(animated, {
+      toValue: 0.6,
+      duration: 100,
+      useNativeDriver: true,
+    }).start();
+  }
+
+  function fadeOut() {
+    Animated.timing(animated, {
+      toValue: 1,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+  }
+
   return (
     <Pressable
       onPress={onPress}
+      onPressIn={fadeIn}
+      onPressOut={fadeOut}
       style={[styles.container, {backgroundColor, width}]}>
-      <Text style={styles.label}>{label}</Text>
+      <Animated.View style={{opacity: animated}}>
+        <Text style={styles.label}>{label}</Text>
+      </Animated.View>
     </Pressable>
   );
 };
