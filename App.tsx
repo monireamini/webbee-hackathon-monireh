3d0f962594851b95982818d5dfcac6c9ac204331
Monire Amini <monireamini@gmail.com>
2022-11-26 19:23:00 +0330

    add drawer

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {persistor, store} from './src/redux/store';
@@ -19,23 +19,28 @@ import Dashboard from './src/modules/dashboard/components/dashboard.screen';
 import ManageCategories from './src/modules/manage-categories/components/manage-categories.screen';
 import CreateCategory from './src/modules/create-category/components/create-category.screen';
 import CreateMachine from './src/modules/create-machine/create-machine.screen';
+import {dimensions} from './src/shared/styles/styles';
 
-const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="ManageCategories">
-            <Stack.Screen name="Dashboard" component={Dashboard} />
-            <Stack.Screen
+          <Drawer.Navigator
+            initialRouteName="Dashboard"
+            screenOptions={{
+              drawerType: dimensions.windowWidth >= 768 ? 'permanent' : 'front',
+            }}>
+            <Drawer.Screen name="Dashboard" component={Dashboard} />
+            <Drawer.Screen
               name="ManageCategories"
               component={ManageCategories}
             />
-            <Stack.Screen name="CreateCategory" component={CreateCategory} />
-            <Stack.Screen name="CreateMachine" component={CreateMachine} />
-          </Stack.Navigator>
+            <Drawer.Screen name="CreateCategory" component={CreateCategory} />
+            <Drawer.Screen name="CreateMachine" component={CreateMachine} />
+          </Drawer.Navigator>
         </NavigationContainer>
       </PersistGate>
     </Provider>
